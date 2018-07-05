257a235d8a5ede350b70ca5ca97a6d0b78aed53c
Tiffany Zheng <qz3@williams.edu>
2018-07-05 09:59:20 -0500

    this is the repo where ill be testing all snippet automation

---
+++ websitedocs/docs/testhome.md
@@ -0,0 +1,27 @@
+---
+id: testsidebarsecond
+title: sidebar second
+sidebar_label: second
+---
+
+Hello this is some markdown text.
+
+<p id="code">
+</p>
+
+<script>
+document.getElementById("code").innerHTML = "testing code";
+</script>
+
+
+<p> does raw html work? </p>
+
+<p>
+``` java
+System.out.println("Does this look like code?");
+
+```
+</p>
+
+
+This is the rest of the tutorial, attempt #2.
--- websitedocs/website/core/Footer.js
+++ websitedocs/website/core/Footer.js
@@ -33,66 +33,40 @@ class Footer extends React.Component {
               />
             )}
           </a>
-          <div>
+         <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href="https://ihmcroboticsdocs.github.io/ihmc-open-robotics-software/docs/quickstarthome.html">
+              Quick Start
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
-            </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+            <a href="https://ihmcroboticsdocs.github.io/docs/docshome.html">
+              Software Documentation
             </a>
           </div>
           <div>
             <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
+            <a href="https://github.com/ihmcrobotics">
+              GitHub
             </a>
             <a
-              href="http://stackoverflow.com/questions/tagged/"
+              href="https://www.facebook.com/TheIHMC"
               target="_blank"
               rel="noreferrer noopener">
-              Stack Overflow
+              Facebook
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
             <a
-              href="https://twitter.com/"
+              href="https://twitter.com/ihmcrobotics"
               target="_blank"
               rel="noreferrer noopener">
               Twitter
             </a>
-          </div>
-          <div>
-            <h5>More</h5>
-            <a href={this.props.config.baseUrl + 'blog'}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count={true}
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
+             <a
+              href="https://www.youtube.com/user/DRCihmcRobotics"
+              target="_blank"
+              rel="noreferrer noopener">
+              YouTube
             </a>
           </div>
         </section>
-
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={this.props.config.baseUrl + 'img/oss_logo.png'}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
